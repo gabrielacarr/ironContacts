cc9623be727a77259049e7a4f26ca6418edb47ea
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-21 21:36:34 -0400

    removed index on img tags, updated notations for multiple functions

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,6 +7,19 @@ class App extends Component {
   state = {
     fiveContacts: contacts.slice(0, 5), // slice to select the first five contacts
   };
+
+  displayFive =  () => {
+    let newArray = this.state.fiveContacts.map(eachContact => {
+    return ( 
+      <tr>
+                  <td><img width="50 px" src={eachContact.pictureUrl} alt={eachContact.name}/> </td>
+                  <td>{eachContact.name}</td>
+                  <td>{eachContact.popularity}</td>
+                  </tr>
+    )
+    })
+    return newArray 
+  }
   render() {
     return (
           <div className="App">
@@ -15,16 +28,18 @@ class App extends Component {
                 <tr>
                   <th>Picture</th>
                   <th>Name</th>
-                  
                   <th>Popularity</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td><img width="50 px" src={this.state.fiveContacts.pictureUrl} alt={this.state.fiveContacts[0].name}/> </td>
-                  <td>{this.state.fiveContacts[0].name}</td>
-                  <td>{this.state.fiveContacts[0].popularity}</td>
-                </tr>
+              {this.displayFive()}
+              {/*
+                // <tr>
+                //   <td><img width="50 px" src={this.state.fiveContacts.pictureUrl} alt={this.state.fiveContacts[0].name}/> </td>
+                //   <td>{this.state.fiveContacts[0].name}</td>
+                //   <td>{this.state.fiveContacts[0].popularity}</td>
+                // </tr> 
+              */}
               </tbody>
             </table>
           </div>
@@ -36,8 +51,7 @@ export default App;
 
 // NOTATIONS FOR APP.JS
 
-// what is a method we can use to cut out a ection of the array
-// line 7
+// what is a method we can use to cut out a section of the array
 // state is an object, create a new property "contact"
 // use splice method to cut of the first five contacts that are going to be used
 // slice returns a sections of an array, comes with two params
@@ -97,8 +111,46 @@ export default App;
 // ADJUSTING WIDTH OF IMG
 // place width inside the img
 // img width="50 px" src==""
+// alt="{eachContact.name}"
 
 // POPULARITY
 
 // same as above, update to popularity rather than name
 // {this.state.fiveContacts[0].popularity} 
+
+// CREATE MULTIPLE FUNCTIONS
+
+// create a function that returns content five times
+// create a function to create loop
+// call is displayFive
+// above render
+// inside of the function,create a new array
+// new array is equal to this.state.fiveContacts.map(eachContact)
+// map mwthod,
+// everytime that it loops through, the variable eachContact will equal one contact at a time
+// as a function, using fat arrow
+// return the repeated code
+// place the copied code that will be repeated inside the return
+// this has now generated a react list
+// instead of using this.state.fiveContact
+// update to eachContact.name
+// EX:
+// return (
+//   <tr>
+//               <td><img width="50 px" src={eachContact.pictureUrl} alt={eachContacts[0].name}/> </td>
+//               <td>{eachContacts[0].name}</td>
+//               <td>{eachContacts[0].popularity}</td>
+//               </tr>
+// make array equal to every element in that array
+// iteration through array
+// and adding map
+// return a new array
+// and each element will populate five js embedded code
+
+// inside body.call the function {this.displayFive()}
+// this displatFive is the class component 
+
+
+// first return, /* returns the map and assign a new array */
+
+// second return, /* returns the function */
